77d6c5d2f24b59185229c1ae3e9b6006d3ca5a22
Evgeniy Tarasov <tarasov@ucontent.ru>
2019-09-21 13:49:04 +0300

    test

--- TarasovEA_HW2.1.2/ViewController.swift
+++ TarasovEA_HW2.1.2/ViewController.swift
@@ -8,15 +8,72 @@
 
 import UIKit
 
+enum CurrentLight {
+    case red
+    case yellow
+    case green
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
+    
     @IBOutlet weak var changeTraficButton: UIButton!
     
-    var step = 0
-
+    // свойство отвечает за текущий цвет
+    private var currentLight = CurrentLight.red
+    
+    // два свойства для состояния
+    //CG - core grafic
+    //
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        changeTraficButton.layer.cornerRadius = 10
+        
+        // присваиваем светофору начальные значения - выключено
+        redLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+    }
+    
+    // переопределяем одно из свойств UIViewController, оно нужно для того, чтобы радиусы были зависимыми от размеров экрана, т.е., это свойство срабатывает не перед самым отображением, когда система уже знает, какой экран у устройства
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    }
+    
+    
+    
+    @IBAction func changeTraficLight() {
+        changeTraficButton.setTitle("NEXT", for: .normal)
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
+    
+    
+    
+    //My home work
+/*----------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,7 +88,7 @@ class ViewController: UIViewController {
         
     }
     
-    
+    var step = 0
 
     @IBAction func changeTraficLight() {
         changeTraficButton.setTitle("NEXT", for: .normal)
@@ -67,6 +124,7 @@ class ViewController: UIViewController {
             
         }
     }
+ ----------------------------------------*/
     
 }
 
